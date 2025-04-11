367c61c63a013dc87edc8ac02eb7a50bfd3a54c5
William Woldum <41169797+williamwoldum@users.noreply.github.com>
2025-04-11 10:32:21 +0200

    Bigger pfp width

--- src/components/Feed/Footer.tsx
+++ src/components/Feed/Footer.tsx
@@ -3,7 +3,7 @@ import footer from '../../assets/instagram/feed/footer.png'
 const Footer = () => {
   return (
     <div className="relative">
-      <div className="absolute inset-0 bg-gradient-to-r ml-auto mr-4 from-teal-400 via-blue-500 to-violet-600 z-0 m-1 w-10" />
+      <div className="absolute inset-0 bg-gradient-to-r ml-auto mr-4 from-teal-400 via-blue-500 to-violet-600 z-0 m-1 w-12" />
       <img src={footer} className="relative w-full pointer-events-none z-1" />
     </div>
   )
--- src/components/Post/Header.tsx
+++ src/components/Post/Header.tsx
@@ -3,7 +3,7 @@ import postHeader from '../../assets/instagram/post/post-header-1.png'
 const Header = () => {
   return (
     <div className="relative">
-      <div className="absolute inset-0 bg-gradient-to-r left-2 from-purple-500 via-pink-500 to-red-500 z-0 m-1 w-10" />
+      <div className="absolute inset-0 bg-gradient-to-r left-2 from-purple-500 via-pink-500 to-red-500 z-0 m-1 w-12" />
       <img src={postHeader} className="relative w-full pointer-events-none z-1" />
     </div>
   )
